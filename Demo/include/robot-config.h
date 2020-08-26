9f2113d64bc03bcc0f6547d230c89a70e070afbe
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-08-26 11:20:21 -0600

    add Demo Code For some reason the VEXCode comiler does not like using .h and .cpp files

---
+++ Demo/include/robot-config.h
@@ -0,0 +1,11 @@
+using namespace vex;
+
+extern brain Brain;
+extern motor test_motor;
+extern PID::PID test_PID;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
---
+++ Demo/src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       ad101-lab                                                 */
+/*    Created:      Tue Aug 11 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+motor test_motor = motor(1, true);
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  PID::PID test = PID::PID(1.0, 0.0, 0.0, test_motor);
+  
+  test.enable();
+
+}
