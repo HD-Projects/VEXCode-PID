bd217dba19cf301a9879930c87a8cfa8dd4a16ff
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2021-02-05 10:29:35 -0700

    Update PID.h

--- Demo/include/PID.h
+++ Demo/include/PID.h
@@ -170,237 +170,181 @@ public:
    */
   int loop();
 };
-//
-// Removing DrivePID temperaroly
-///**
-// * DrivePID.
-// *
-// * @author ad101-lab
-// * @since	v0.0.1
-// * @version	v1.0.0	Tuesday, August 25th, 2020.
-// */
-// class DrivePID {
-//  public:
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 12:58:47
-//     * @drawer_cat{constructor}
-//     * @brief: A constructor for a drive PID
-//     * @param P Proportional
-//     * @param I Integral
-//     * @param D Derivative
-//     * @param turnP Turn Proportional
-//     * @param turnI Turn Integral
-//     * @param turnD Turn Derivative
-//     * @param left Left motor_group for the left side of this drivetrain
-//     * @param right Right motor_group for the left side of this drivetrain
-//     * @param turnIntertial Default=21 inertial that you use for turning
-//     * @param wheelDiameter (Optional) the diameter of your wheels
-//     * @param sleep (Optional) for advanced users change the sleep time per
-//     loop
-//     */
-//    DrivePID( double P, double I, double D, double turnP, double turnI, double
-//    turnD, motor_group left, motor_group right, inertial turnIntertial, double
-//    wheelDiameter, int sleep = 20 );
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 12:58:47
-//     * @drawer_cat{constructor}
-//     * @brief: A constructor for a drive PID Sets bot turn and lateral kP, kI
-//     and kD to the same thing
-//     * @param P Proportional
-//     * @param I Integral
-//     * @param D Derivative
-//     * @param left Left motor_group for the left side of this drivetrain
-//     * @param right Right motor_group for the left side of this drivetrain
-//     * @param turnIntertial inertial that you use for turning
-//     * @param wheelDiameter (Optional) the diameter of your wheels
-//     * @param sleep (Optional) for advanced users change the sleep time per
-//     loop
-//     */
-//    DrivePID( double P, double I, double D, motor_group left, motor_group
-//    right, inertial turnIntertial, double wheelDiameter, int sleep = 20 );
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 12:58:47
-//     * @drawer_cat{constructor}
-//     * @brief: A constructor for a drive Still need to set all of the values.
-//     */
-//    DrivePID();
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 14:31:16
-//     * @brief Enable the object of PID
-//     */
-//    void enable();
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 14:31:16
-//     * @brief Disable the object of PID
-//     */
-//    void disable();
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 14:31:16
-//     * @brief Reset the sensors
-//     */
-//    void reset();
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 14:33:44
-//     * @brief Move a selected distance
-//     * @param distance Distance to move
-//     */
-//    void move(double distance);
-//
-//    /**
-//     * @Author: ad101-lab
-//     * @Date: 2020-08-25 14:36:48
-//     * @brief: Call this function in a vex::task instance to run the PID
-//     */
-//    int loop();
-//
-//    /**
-//     * @var		double	kP
-//     */
-//    double kP;
-//
-//    /**
-//     * @var		double	kI
-//     */
-//    double kI;
-//
-//    /**
-//     * @var		double	kD
-//     */
-//    double kD;
-//
-//    /**
-//     * @var		double	turnkP
-//     */
-//    double turnkP;
-//
-//    /**
-//     * @var		double	turnkI
-//     */
-//    double turnkI;
-//
-//    /**
-//     * @var		double	turnkD
-//     */
-//    double turnkD;
-//
-//    /**
-//     * @var		motor_group	leftMotors
-//     */
-//    motor_group leftMotors;
-//
-//    /**
-//     * @var		motor_group	rightMotors
-//     */
-//    motor_group rightMotors;
-//
-//    /**
-//     * @var		inertial	turning
-//     */
-//    inertial turning;
-//
-//    /**
-//     * @var		bool	enabled
-//     */
-//    bool enabled;
-//
-//    /**
-//     * @var		bool	resetSensors
-//     */
-//    bool resetSensors;
-//
-//
-//    /**
-//     * @var		int	sleepTime
-//     */
-//    int sleepTime;
-//
-//    /**
-//     * @var		int	maxIntegral
-//     */
-//    int maxIntegral;
-//
-//    /**
-//     * @var		int	maxTurnIntegral
-//     */
-//    int maxTurnIntegral;
-//
-//    /**
-//     * @var		int	integralBound
-//     */
-//    int integralBound;
-//
-//    /**
-//     * sensorValue - DesiredValue : Positional
-//     *
-//     * @var		int	error
-//     */
-//    int error;
-//
-//    /**
-//     * Error the previous loop
-//     *
-//     * @var		int	prevError
-//     */
-//    int prevError;
-//
-//    /**
-//     * @var		int	derivative
-//     */
-//    int derivative;
-//
-//    /**
-//     * @var		int	totalError
-//     */
-//    int totalError;
-//
-//    /**
-//     * sensorValue - DesiredValue : Positional
-//     *
-//     * @var		int	turnError
-//     */
-//    int turnError;
-//
-//    /**
-//     * Error the previous loop
-//     *
-//     * @var		integer	turnPrevError
-//     */
-//    int turnPrevError = 0;
-//
-//    /**
-//     * @var		int	turnDerivative
-//     */
-//    int turnDerivative;
-//    /**
-//     * @var		integer	turnTotalError
-//     */
-//    int turnTotalError = 0;
-//
-//    /**
-//     * Initialize distance
-//     *
-//     * @var		int	desiredValue
-//     */
-//    int desiredValue;
-//
-//    /**
-//     * @var		int	desiredTurnValue
-//     */
-//    int desiredTurnValue;
-//};
+
+/**
+ * DrivePID.
+ *
+ * @author ad101-lab
+ * @since	v0.0.1
+ * @version	v1.0.0	Tuesday, August 25th, 2020.
+ */
+class DrivePID {
+public:
+  // Create doubles for P, I and D
+  double kP;
+  double kI;
+  double kD;
+
+  // turning
+  double turnkP;
+  double turnkI;
+  double turnkD;
+
+  // Left and Right motorgroups
+  motor_group leftMotors;
+  motor_group rightMotors;
+
+  // Sensors
+  inertial turning = inertial(PORT11);
+
+  // Varable to keep track of "on" state
+  bool enabled = false;
+
+  // Reset Sensor Reset Value
+  bool resetSensors = false;
+
+  // Advanced
+
+  int sleepTime;         // The time that the loop sleeps each time.
+  int maxIntegral = 300; // Max out integral to prevent
+  int maxTurnIntegral = 300;
+  int integralBound = 3;
+
+  int error;         // sensorValue - DesiredValue : Positional
+  int prevError = 0; // Error the previous loop
+  int derivative;    //
+  int totalError = 0;
+
+  int turnError;         // sensorValue - DesiredValue : Positional
+  int turnPrevError = 0; // Error the previous loop
+  int turnDerivative;    //
+  int turnTotalError = 0;
+
+  // Initialize distance
+  int desiredValue;
+  int desiredTurnValue;
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 12:58:47
+   * @drawer_cat{constructor}
+   * @brief: A constructor for a drive PID
+   * @param P Proportional
+   * @param I Integral
+   * @param D Derivative
+   * @param turnP Turn Proportional
+   * @param turnI Turn Integral
+   * @param turnD Turn Derivative
+   * @param left Left motor_group for the left side of this drivetrain
+   * @param right Right motor_group for the left side of this drivetrain
+   * @param turnIntertial Default=21 inertial that you use for turning
+   * @param wheelDiameter (Optional) the diameter of your wheels
+   * @param sleep (Optional) for advanced users change the sleep time per
+   loop
+   */
+  DrivePID(double P, double I, double D, double turnP, double turnI,
+           double turnD, motor_group left, motor_group right, int turnIntertial,
+           double wheelDiameter, int sleep = 20) {
+    // Set P, I and D
+    kP = P;
+    kI = I;
+    kD = D;
+
+    turnkP = turnP;
+    turnkI = turnI;
+    turnkD = turnD;
+
+    // Set motors
+    leftMotors = left;
+    rightMotors = right;
+  }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 12:58:47
+   * @drawer_cat{constructor}
+   * @brief: A constructor for a drive PID Sets bot turn and lateral kP, kI
+   and kD to the same thing
+   * @param P Proportional
+   * @param I Integral
+   * @param D Derivative
+   * @param left Left motor_group for the left side of this drivetrain
+   * @param right Right motor_group for the left side of this drivetrain
+   * @param turnIntertial inertial that you use for turning
+   * @param wheelDiameter (Optional) the diameter of your wheels
+   * @param sleep (Optional) for advanced users change the sleep time per
+   loop
+   */
+  DrivePID(double P, double I, double D, motor_group left, motor_group right,
+           int turnIntertial, double wheelDiameter, int sleep = 20) {
+    // Set P, I and D
+    kP, turnkP = P;
+    kI, turnkI = I;
+    kD, turnkD = D;
+
+    // Set motors
+    leftMotors = left;
+    rightMotors = right;
+  }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 12:58:47
+   * @drawer_cat{constructor}
+   * @brief: A constructor for a drive Still need to set all of the values.
+   */
+  DrivePID() {}
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 14:31:16
+   * @brief Enable the object of PID
+   */
+  void enable() { enabled = true; }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 14:31:16
+   * @brief Disable the object of PID
+   */
+  void disable() { enabled = false; }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 14:31:16
+   * @brief Reset the sensors
+   */
+  void reset() { resetSensors = true; }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 14:33:44
+   * @brief Move a selected distance
+   * @param distance Distance to move
+   */
+  void move(double distance) {
+    // Reset the drive sensors
+    resetSensors = true;
+
+    // Set desired value
+    desiredValue = distance;
+  }
+
+  void turn(double degrees) {
+    // Reset the drive sensors
+    resetSensors = true;
+
+    // Set desired value
+    desiredTurnValue = degrees;
+  }
+
+  /**
+   * @Author: ad101-lab
+   * @Date: 2020-08-25 14:36:48
+   * @brief: Call this function in a vex::task instance to run the PID
+   */
+  int loop();
+};
 }; // namespace PID
 
 #endif
